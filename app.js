5916db2e5f33cb1ce6bfcabdf59c1aabee226474
Ivan <ivaanlml@gmail.com>
2021-06-11 18:50:35 -0300

    Pantalla cuadno no hay tareas y indexedDB implementado

--- app.js
+++ app.js
@@ -30,64 +30,74 @@ class AdmTarea {
 }
 
 class UI {
-    imprimirtareas({ tareas }) {
-        //const { tarea } = tareas;
-
+    imprimirTareas() {
         limpiarHTML();
-        tareas.forEach((task) => {
-            // Contenedor de la tarea.
-            const div = document.createElement("div");
-            div.classList.add("divTarea");
-            div.innerHTML = `<p class="tarea" > <span>${task.tarea}</span>  </p>`;
+        const objectStore = DB.transaction("tasks", "readonly").objectStore(
+            "tasks"
+        );
+        objectStore.openCursor().onsuccess = (e) => {
+            const cursor = e.target.result;
 
-            // Icono check
-            const checkButton = document.createElement("span");
-            checkButton.innerHTML = `<svg width="25px" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>`;
-            checkButton.classList.add("check");
+            if (cursor) {
+                const { tarea, id } = cursor.value;
+                // Contenedor de la tarea.
+                const div = document.createElement("div");
+                div.classList.add("divTarea");
+                div.innerHTML = `<p class="tarea" > <span>${tarea}</span>  </p>`;
 
-            // Checkbox
-            const checkBox = document.createElement("input");
-            checkBox.type = "checkbox";
-            checkBox.classList.add("checkBox");
-            checkBox.addEventListener("click", () => {
-                if (checkBox.checked) {
-                    checkButton.style.color = "#00af91";
-                } else {
-                    checkButton.style.color = "lightslategray";
-                }
-            });
-            //Contenedor de botones.
-            const divBotones = document.createElement("div");
+                // Icono check
+                const checkButton = document.createElement("span");
+                checkButton.innerHTML = `<svg width="25px" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>`;
+                checkButton.classList.add("check");
 
-            //Boton para borrar.
-            const btnDelete = document.createElement("button");
-            btnDelete.innerHTML = `&times Borrar`;
-            btnDelete.classList.add("btnDelete");
-            btnDelete.setAttribute("id", "btnDelete");
-            divBotones.appendChild(btnDelete);
-            btnDelete.onclick = () => {
-                eliminarTarea(task.id);
-            };
+                // Checkbox
+                const checkBox = document.createElement("input");
+                checkBox.type = "checkbox";
+                checkBox.classList.add("checkBox");
+                checkBox.addEventListener("click", () => {
+                    if (checkBox.checked) {
+                        checkButton.style.color = "#00af91";
+                    } else {
+                        checkButton.style.color = "lightslategray";
+                    }
+                });
+                //Contenedor de botones.
+                const divBotones = document.createElement("div");
 
-            const btnEditar = document.createElement("button");
-            btnEditar.innerHTML = `<svg width="14" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-            </svg> Editar`;
-            btnEditar.classList.add("btnEditar");
-            btnEditar.setAttribute("id", "btnEditar");
-            divBotones.appendChild(btnEditar);
-            btnEditar.onclick = () => {
-                editarTarea(task);
-            };
+                //Boton para borrar.
+                const btnDelete = document.createElement("button");
+                btnDelete.innerHTML = `&times Borrar`;
+                btnDelete.classList.add("btnDelete");
+                btnDelete.setAttribute("id", "btnDelete");
+                divBotones.appendChild(btnDelete);
+                btnDelete.onclick = () => {
+                    eliminarTarea(id);
+                };
 
-            div.appendChild(divBotones);
-            div.appendChild(checkButton);
-            div.appendChild(checkBox);
+                // Boton para editar.
+                const btnEditar = document.createElement("button");
+                btnEditar.innerHTML = `<svg width="14" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                </svg> Editar`;
+                btnEditar.classList.add("btnEditar");
+                btnEditar.setAttribute("id", "btnEditar");
+                divBotones.appendChild(btnEditar);
+                const tare = cursor.value;
+                btnEditar.onclick = () => {
+                    editarTarea(tare);
+                };
 
-            taskContainer.appendChild(div);
-        });
+                div.appendChild(divBotones);
+                div.appendChild(checkButton);
+                div.appendChild(checkBox);
+
+                taskContainer.appendChild(div);
+                cursor.continue();
+            }
+            compruebaVacio();
+        };
     }
     mostrarAlerta(mensaje, tipo) {
         const alerta = document.querySelector("#alerta"); // Se selecciona el contenedor del mensaje.
@@ -133,13 +143,20 @@ const admTarea = new AdmTarea();
 
 // Al cargar la pagina se ejecutan las funciones.
 document.addEventListener("DOMContentLoaded", () => {
+    // Añadir eventos.
+    addEventListeners();
+
     //crearDB();
+    crearDB();
+});
+
+function addEventListeners() {
     taskName.addEventListener("input", llenarObj);
     taskArea.addEventListener("input", llenarObj);
 
     form.addEventListener("submit", nuevaTarea);
     btnGuardar.addEventListener("click", nuevaTarea);
-});
+}
 
 function limpiarHTML() {
     while (taskContainer.firstChild) {
@@ -160,17 +177,27 @@ function nuevaTarea(e) {
     }
     if (editando) {
         admTarea.editTarea({ ...tareasObj });
-        ui.mostrarAlerta("Tarea actualizada.", "success");
-        pantallaEdicion.style.display = "none";
-        editando = false;
+
+        const transaction = DB.transaction(["tasks"], "readwrite");
+        const objectStore = transaction.objectStore("tasks");
+        objectStore.put(tareasObj);
+        transaction.oncomplete = () => {
+            ui.mostrarAlerta("Tarea actualizada.", "success");
+            pantallaEdicion.style.display = "none";
+            editando = false;
+        };
+        transaction.onerror = () => {
+            console.log("Hubo un error.");
+        };
     } else {
         tareasObj.id = Date.now();
         admTarea.agregarTarea({ ...tareasObj });
-        ui.mostrarAlerta("Tarea Creada", "success");
+        // Almacena datos en indexedDB
+        postDB(tareasObj);
     }
 
     form.reset();
-    ui.imprimirtareas(admTarea);
+    ui.imprimirTareas();
 
     reiniciarObjeto();
 }
@@ -180,9 +207,18 @@ function reiniciarObjeto() {
 }
 
 function eliminarTarea(id) {
-    admTarea.deleteTarea(id);
-    ui.imprimirtareas(admTarea);
-    ui.mostrarAlerta("Tarea Eliminada", "error");
+    const transaction = DB.transaction(["tasks"], "readwrite");
+    const objectStore = transaction.objectStore("tasks");
+
+    objectStore.delete(id);
+
+    transaction.oncomplete = () => {
+        ui.imprimirTareas();
+        ui.mostrarAlerta("Tarea Eliminada", "error");
+    };
+    transaction.onerror = () => {
+        console.log("Hubo un error");
+    };
 }
 function editarTarea(task) {
     const { tarea, id } = task;
@@ -202,7 +238,19 @@ function editarTarea(task) {
 
     editando = true;
 }
-/* function crearDB() {
+
+function compruebaVacio() {
+    const contSinTareas = document.querySelector(".contSinTareas");
+    if (!taskContainer.firstChild) {
+        taskContainer.style.display = "none";
+        contSinTareas.style.display = "flex";
+    } else {
+        taskContainer.style.display = "grid";
+        contSinTareas.style.display = "none";
+    }
+}
+
+function crearDB() {
     let taskDB = window.indexedDB.open("tasks", 1);
 
     taskDB.onerror = () => {
@@ -210,41 +258,38 @@ function editarTarea(task) {
     };
     taskDB.onsuccess = () => {
         console.log("La DB se creo correctamente");
-        DB = taskDB;
-        /getDB(); 
+        DB = taskDB.result;
+        ui.imprimirTareas();
     };
     taskDB.onupgradeneeded = (e) => {
         const data = e.target.result;
 
         const objectStore = data.createObjectStore("tasks", {
-            keyPath: "tasks",
+            keyPath: "id",
             autoIncrement: true,
         });
         objectStore.createIndex("tarea", "tarea", { unique: false });
         objectStore.createIndex("id", "id", { unique: true });
     };
-} */
-/* 
-function postDB() {
-    let transaction = DB.result.transaction(["tasks"], "readwrite");
+}
 
-    transaction.oncomplete = function () {
-        console.log("Conexion con  la base de datos exitosa.");
-    };
+function postDB(tareasObj) {
+    const transaction = DB.transaction(["tasks"], "readwrite");
+    const objectStore = transaction.objectStore("tasks");
+    objectStore.add(tareasObj);
     transaction.onerror = function () {
         console.log("Error al conectar con la Base de datos.");
     };
-    const objectStore = transaction.objectStore("tasks");
+    transaction.oncomplete = () => {
+        console.log("cita agregada");
 
-    const tareasObj = { ...tareaObj };
-
-    peticion = objectStore.add(tareasObj);
-
-    ui.imprimirtarea(tareasObj);
-} */
+        //Muestra el mensaje de confirmacion del almacenamiento
+        ui.mostrarAlerta("Tarea Creada", "success");
+    };
+}
 
 /* function getDB() {
-    const transaction = DB.result.transaction(["tasks"], "readonly");
+    const transaction = DB.transaction(["tasks"], "readonly");
 
     const objectStore = transaction.objectStore("tasks");
 
@@ -253,19 +298,11 @@ function postDB() {
     request.onsuccess = (e) => {
         const cursor = e.target.result;
         if (cursor) {
-            ui.imprimirtarea(cursor.value);
+            ui.imprimirtareas(cursor.value);
+
             cursor.continue();
         } else {
             console.log("Sin datos");
         }
     };
 } */
-
-/*
-function guardarEdicion(tarea) {
-    const btnGuardar = document.querySelector(".btnGuardar");
-    btnGuardar.addEventListener("click", (e) => {
-        console.log(e.target);
-    });
-}
- */
